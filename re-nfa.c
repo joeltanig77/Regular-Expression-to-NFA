69fd4e7adb060a67c99cf1db3078f506d7105d0e
Joel Tanig <joeltanig77@gmail.com>
2021-09-30 19:46:22 -0700

    adding to the & case

--- re-nfa.c
+++ re-nfa.c
@@ -14,10 +14,16 @@ struct Stack {
 struct NFA {
   int startState;
   int acceptState;
+  int size;
+  char symbol;
+  struct Transitions * tran;
+};
+
+struct Transitions {
   int stateOne;
   int stateTwo;
   char symbol;
-  struct NFA * connect;
+  struct Transitions * tran;
 };
 
 int push(struct Stack* stack, struct NFA* nfa) {
@@ -32,10 +38,10 @@ struct NFA* pop(struct Stack* stack) {
 
 
 int main(int argc,char *argv[]) {
-  int regIncrementor = 0;
   char line[256];
   memset(line, '\0', sizeof(line));
   int index = 0;
+  int stateCounter = 0;
   struct Stack* stack = (struct Stack*)calloc(100000000,sizeof(stack));
   if (!stack) {
     fprintf(stderr,"%s\n","Failed to allocate memory");
@@ -49,7 +55,7 @@ int main(int argc,char *argv[]) {
   }
 
   if(argc == 1) {
-    fprintf(stdout,"%s\n", "No regular expression was passed in");
+    fprintf(stdout,"%s\n", "No file was passed in");
     free(stack);
     free(nfaArray);
     return 0;
@@ -73,9 +79,19 @@ int main(int argc,char *argv[]) {
       index += 1;
       // if the char == '&'
       if (c == 38) {
-        struct NFA* nfa2 = pop(stack);
-        struct NFA* nfa1 = pop(stack);
-        // Do shit
+        struct NFA* nfa2 = pop(stack); // b
+        struct NFA* nfa1 = pop(stack); // a
+        //printf("\n");
+        struct NFA* added = (struct NFA*)calloc(1,sizeof(added));
+        struct Transitions* tran = (struct Transitions*)calloc(1,sizeof(tran));
+        added->tran = tran;
+        added->tran->stateOne = nfa1->acceptState;
+        added->tran->stateTwo = nfa2->startState;
+        added->tran->symbol = 'E';
+        added->startState = nfa1->startState;
+        added->acceptState = nfa2->acceptState;
+        push(stack,added);
+        // NEED TO DEBUG TO SEE IF THIS FULLY WORKS
       }
       // if the char == '|'
       else if (c == 124) {
@@ -91,12 +107,22 @@ int main(int argc,char *argv[]) {
       }
       else {
         // Push
+        // If c is a new line, continue
+        if (c == 10) {
+          continue;
+        }
         struct NFA* nfa = (struct NFA*)calloc(1,sizeof(nfa));
-        nfa->startState = 0;
-        nfa->acceptState = 1;
-        nfa->stateOne = 0;
-        nfa->stateTwo = 1;
+        struct Transitions* tran = (struct Transitions*)calloc(1,sizeof(tran));
+        nfa->tran = tran;
+        nfa->tran->stateOne = stateCounter;
+        nfa->startState = stateCounter;
+        stateCounter += 1;
+        nfa->tran->stateTwo = stateCounter;
+        nfa->acceptState = stateCounter;
+        stateCounter += 1;
+        //stateCounter += 1;
         nfa->symbol = c;
+        nfa->size += 2;
         push(stack,nfa);
       }
     }
